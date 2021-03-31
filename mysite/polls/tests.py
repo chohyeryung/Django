25c57dd20835fc99871297feef61c3c792632fc4
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-31 09:27:03 +0900

    make test question

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,10 +1,10 @@
 import datetime
 
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import Question
-
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
@@ -22,3 +22,40 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59) #23시간 59분 59초 전
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)   #지금으로부터 며칠후에
+    return Question.objects.create(question_text=question_text, pub_date=time)  #미래에 question 만드는
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+
+        self.assertQuerysetEqual(response.context['lastest_question_list'], [])
+
+    def test_past_question(self):
+        create_question(question_text="Past question.", days=-30)   #30일 전
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['lastest_question_list'], ['<Question:Past question.>'])
+
+    def test_future_question(self):
+        create_question(question_text="Future question.", days=30)  #30일 후
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+
+        self.assertQuerysetEqual(response.context['lastest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        create_question(question_text='Past question.', days=-30)    #30일 전
+        create_question(question_text='Future question.', days=30)  #30일 후
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question:Past question.>'])
+
+    def test_two_past_question(self):
+        create_question(question_text='Past question 1.', days=-30)    #30일 전
+        create_question(question_text='Past question 2.', days=-5)   #5일 전
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['lastest_question_list'], ['<Question:Past question 2.>', '<Question: Past question 1.>'])
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question, Choice
@@ -10,8 +11,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        # lte는 이하라는 뜻
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
